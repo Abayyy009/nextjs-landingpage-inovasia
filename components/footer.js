6ec7236c425fe1dbae1452995072a377bd397d4d
Abayyy009 <160494830+Abayyy009@users.noreply.github.com>
2025-05-23 11:13:02 +0700

    Delete components directory

--- components/footer.js
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="py-6 text-center text-white bg-black/50">
-      <p>
-        &copy; {new Date().getFullYear()} Inovasi Course. All rights reserved.
-      </p>
-    </footer>
-  );
-}
